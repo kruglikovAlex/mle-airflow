6d2a6d5573ed474bfd7143b2261191e8e0051a13
kruglikovAlex <kruglikovalex280577@gmail.com>
2025-06-09 08:06:31 +0000

    fit model

---
+++ practicum_files/read_model_pkl.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import io
+import joblib
+
+fitted_model = joblib.load('/home/mle-user/mle_projects/mle-airflow/fitted_model.pkl') #pd.read_pickle('/home/mle-user/mle_projects/mle-airflow/fitted_model.pkl')
